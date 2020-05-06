d55217853ca12144df5441e2fb519bcd85bb619a
Yu YAGIHASHI <yagihash@xss.moe>
2020-05-06 22:23:43 +0900

    add token

---
+++ token/token.go
@@ -0,0 +1,26 @@
+package token
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+type TokenType string
+
+const (
+	// Token definitions for lexical analysis
+	ILLEGAL   = "ILLEGAL"
+	EOF       = "EOF"
+	IDENT     = "IDENT"
+	INT       = "INT"
+	ASSIGN    = "="
+	PLUS      = "+"
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+	FUNCTION  = "FUNCTION"
+	LET       = "LET"
+)
