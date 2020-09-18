8cf5b29ebd893c3dba7cfd0e0720df3838570082
Yu YAGIHASHI <yagihash@xss.moe>
2020-09-18 19:10:06 +0900

    chapter 2

--- repl/repl.go
+++ repl/repl.go
@@ -5,8 +5,9 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/yagihash/monkey/parser"
+
 	"github.com/yagihash/monkey/lexer"
-	"github.com/yagihash/monkey/token"
 )
 
 const PROMPT = ">> "
@@ -23,9 +24,21 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
